a7709a1cbe6499d70fc91f0ce70c6ac37dd1bfeb
irfankhan309 <irfanqehs@gmail.com>
2019-11-04 16:35:25 +0530

    today add for reminding

--- Ai.py
+++ Ai.py
@@ -3,13 +3,17 @@ import datetime
 import speech_recognition as sr
 import webbrowser
 import time
-
+import datetime
 
 
 
 engine =  pyttsx3.init('sapi5') # to take voice from enduser... its using windows api
 voices = engine.getProperty('voices') # it having the voices 
 engine.setProperty('voice',voices[0].id)   # selecting the voice male or female...
+
+# current date
+today = datetime.datetime.now()
+
 
 def speak(audio):
     engine.say(audio)
@@ -21,7 +25,7 @@ def wishMe():
     speak('i am jarvis,Personal Assistant How May i help you.')
     time.sleep(5)
     speak("still you thinking what to say right?")
-
+    speak(today,"you have some thing to do now")
 
 
 def take_command():
